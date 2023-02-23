cbed16730f2bf18f5da6378fdb006ffa1742ba01
Sandro Young <ord-nas@users.noreply.github.com>
2023-02-22 22:24:39 -0800

    Proof of concept multipart messages.

--- crow/fractal_server.cpp
+++ crow/fractal_server.cpp
@@ -664,7 +664,42 @@ int main() {
 	 png = GeneratePng<double>(*fractal_params, worker_pool);
 	 break;
       }
-      return crow::response("png", png);
+
+      crow::multipart::part png_part;
+      {
+	png_part.body = png;
+	crow::multipart::header content_type_header;
+	content_type_header.value = "image/png";
+	crow::multipart::header content_disposition_header;
+	content_disposition_header.value = "form-data";
+	content_disposition_header.params["name"] = "fractal_image";
+	content_disposition_header.params["filename"] = "fractal_image.png";
+	png_part.headers.emplace("Content-Type", content_type_header);
+	png_part.headers.emplace("Content-Disposition", content_disposition_header);
+      }
+
+      crow::multipart::part metadata_part;
+      metadata_part.body = "This is some additional data!";
+      {
+	crow::json::wvalue metadata({{"message", "Hello, World!"},
+				     {"nested_object", {
+					 {"a", 1},
+					 {"b", 42},
+				       }},
+				     {"list", std::vector<crow::json::wvalue>{1,2,3}}});
+	metadata_part.body = metadata.dump();
+	crow::multipart::header content_type_header;
+	content_type_header.value = "application/json";
+	crow::multipart::header content_disposition_header;
+	content_disposition_header.value = "form-data";
+	content_disposition_header.params["name"] = "metadata";
+	metadata_part.headers.emplace("Content-Type", content_type_header);
+	metadata_part.headers.emplace("Content-Disposition", content_disposition_header);
+      }
+
+      return crow::response(crow::multipart::message(/*header=*/{},
+						     /*boudary=*/"CROW-BOUNDARY",
+						     /*parts=*/{png_part, metadata_part}));
     });
 
   // Test page - image cycler.
